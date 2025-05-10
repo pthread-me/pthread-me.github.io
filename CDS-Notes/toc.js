6f6d85f9df4172ab69f5bb90fee8b7566d1ccafc
pthread-me <anas.n.alhadi@outlook.com>
2025-05-10 18:12:05 -0300

    test done

--- CDS-Notes/toc.js
+++ CDS-Notes/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="bitvec.html"><strong aria-hidden="true">1.</strong> Bit vectors</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="bitvec.html"><strong aria-hidden="true">1.</strong> Bit Vectors</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
